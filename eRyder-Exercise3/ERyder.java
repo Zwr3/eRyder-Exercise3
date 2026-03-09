fa85a4396b8c51b0bbf684ce4024ae8e9c6c971b
Zwr3 <2308586759@qq.com>
2026-03-09 18:23:18 +0800

    Finish Exercise 3 by myself again

--- eRyder-Exercise3/ERyder.java
+++ eRyder-Exercise3/ERyder.java
@@ -1,35 +1,42 @@
 public class ERyder {
-    private String bikeID; 
-    private int batteryLevel; 
-    private boolean isAvailable; 
-    private double kmDriven; 
+    private String bikeID;
+    private int batteryLevel;
+    private boolean isAvailable;
+    private double kmDriven;
+
     public ERyder() {
-        this.bikeID = "default-001";
-        this.batteryLevel = 0;
-        this.isAvailable = false;
-        this.kmDriven = 0.0;
+        bikeID = "default-001";
+        batteryLevel = 0;
+        isAvailable = false;
+        kmDriven = 0.0;
     }
+
     public ERyder(String bikeID, int batteryLevel, boolean isAvailable, double kmDriven) {
         this.bikeID = bikeID;
         this.setBatteryLevel(batteryLevel);
         this.isAvailable = isAvailable;
         this.kmDriven = kmDriven;
     }
+
     public void ride() {
-        if (this.batteryLevel > 0 && this.isAvailable) {
+        boolean canRide = batteryLevel > 0 && isAvailable;
+        if (canRide) {
             System.out.println("The bike is available for riding.");
         } else {
             System.out.println("The bike is not available for riding.");
         }
     }
+
     public void printBikeDetails() {
         System.out.println("===== Bike Details =====");
-        System.out.println("Bike ID: " + this.bikeID);
-        System.out.println("Battery Level: " + this.batteryLevel + "%");
-        System.out.println("Is Available: " + (this.isAvailable ? "Yes" : "No"));
+        System.out.println("Bike ID: " + bikeID);
+        System.out.println("Battery Level: " + batteryLevel + "%");
+        String availableText = isAvailable ? "Yes" : "No";
+        System.out.println("Is Available: " + availableText);
         System.out.println("Total KM Driven: " + this.kmDriven + " km");
         System.out.println("========================");
     }
+
     public void setBatteryLevel(int batteryLevel) {
         if (batteryLevel >= 0 && batteryLevel <= 100) {
             this.batteryLevel = batteryLevel;
@@ -38,8 +45,13 @@ public class ERyder {
             this.batteryLevel = 0;
         }
     }
+
     public String getBikeID() {
         return bikeID;
+    }
+
+    public void setBikeID(String bikeID) {
+        this.bikeID = bikeID;
     }
 
     public int getBatteryLevel() {
@@ -50,7 +62,15 @@ public class ERyder {
         return isAvailable;
     }
 
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
     public double getKmDriven() {
         return kmDriven;
+    }
+
+    public void setKmDriven(double kmDriven) {
+        this.kmDriven = kmDriven;
     }
 }
--- eRyder-Exercise3/Main.java
+++ eRyder-Exercise3/Main.java
@@ -1,11 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         ERyder bike1 = new ERyder();
-        System.out.println("--- Bike 1 (Default Constructor) ---");
+        System.out.println("Testing bike1:");
+        bike1.ride();
         bike1.printBikeDetails();
-        ERyder bike2 = new ERyder("EB-2026", 80, true, 156.8);
-        System.out.println("\n--- Bike 2 (Parameterized Constructor) ---");
-        bike2.ride(); 
-        bike2.printBikeDetails(); 
+        System.out.println();
+
+        ERyder bike2 = new ERyder("EB-2026", 85, true, 120.5);
+        System.out.println("Testing bike2:");
+        bike2.ride();
+        bike2.printBikeDetails();
+        System.out.println();
+
+        ERyder bike3 = new ERyder("EB-9999", 150, false, 50.0);
+        System.out.println("Testing bike3 before change:");
+        bike3.ride();
+        
+        bike3.setBatteryLevel(70);
+        bike3.setAvailable(true);
+        bike3.setKmDriven(60.8);
+        
+        System.out.println("Testing bike3 after change:");
+        bike3.ride();
+        bike3.printBikeDetails();
     }
 }
