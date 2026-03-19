48797b7d012a6f44aac15135a5c7dae09a04f6fe
Zwr3 <2308586759@qq.com>
2026-03-19 14:02:37 +0800

    Finish the excercise6

---
+++ eRyder-Exercise3/Excercise6/Main.java
@@ -0,0 +1,8 @@
+package Excercise6;
+class Main {
+    public static void main(String[] args) {
+        UserRegistration userReg = new UserRegistration();
+        userReg.registration();
+        System.out.println(userReg);
+    }
+}
